abefa0c0fc44935433bae201a2aa725aca9469f0
Pratoosh-18 <pratoosh10garg@gmail.com>
2024-06-17 23:57:54 +0530

    added the util files

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,10 @@
 import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
-const helloUser = (req,res) => {
+const helloUser = (req, res) => {
     console.log("Hello user")
+
 }
 
-export {helloUser}
+export { helloUser }
