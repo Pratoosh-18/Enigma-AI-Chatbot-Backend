ce4c50695ae93491a680001203e66e7df5f1fdc6
Pratoosh-18 <pratoosh10garg@gmail.com>
2024-06-26 21:54:53 +0530

    Added with credentials property

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ import express from "express"
 import cookieParser from "cookie-parser"
 
 const app = express()
-app.use(cors())
+
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true
+}
+app.use(cors(corsOptions))
 
 app.use(express.json())
 app.use(express.urlencoded())
