f3782fe56650e720202dc02e68a73bebe091fd6b
Pratoosh-18 <pratoosh10garg@gmail.com>
2024-06-28 01:45:41 +0530

    updated cors

--- app.js
+++ app.js
@@ -4,11 +4,7 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
-const corsOptions = {
-    origin: 'http://localhost:5173',
-    credentials: true
-}
-app.use(cors(corsOptions))
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded())
